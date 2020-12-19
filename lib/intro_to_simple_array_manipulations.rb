f05d595b38ac56b64be2400c3d4af2e0aa64b86a
eager-monitor-013e <phil.haines92@gmail.com>
2020-12-19 21:34:57 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,18 @@
+require "pry"
+
+def using_push(array, string)
+ arr = ["Hi", "There", "Friend"]
+ arr.push("violet")
+ arr
+end
+
+def using_unshift(array, string)
+ arr = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+ arr.unshift("Staten Island")
+end
+
+def using_pop(array)
+  continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+  continents.pop
+  binding.pry
+end

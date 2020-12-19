fdcdf19f18f6a1525732acfae6c80489e2e8657d
eager-monitor-013e <phil.haines92@gmail.com>
2020-12-19 21:56:40 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -13,6 +13,5 @@ end
 
 def using_pop(array)
   continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
-  continents.pop
-  binding.pry
+  continents.pop("Antarctica")
 end
